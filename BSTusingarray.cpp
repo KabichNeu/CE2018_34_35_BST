b10fb77c56527db7fbc195fb4017bf552c608f42
SujanNepal1234567 <sujannepal11111@gmail.com>
2020-01-30 10:14:47 +0545

    Array with add,traversal and search

---
+++ BSTusingarray.cpp
@@ -0,0 +1,21 @@
+#include"BST.h" 
+#define MAX_SIZE 1000
+
+class ArrayBST: public BST
+{
+		public :
+				void add(int data);
+				void preordertraversal(); 
+				bool search(int data); 
+				)
+			private:
+				 int data[MAX_SIZE]; 
+				 
+};
+
+
+void ArrayBST:: add(int data)
+{
+	
+	
+ } 
