810c249847978773bc31cd24d275987fa40e4dd8
kabichneu <kabichneu@gmail.com>
2020-01-30 10:58:39 +0545

    second commit

---
+++ linkedBST.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "linkedBST.cpp"
+
+
+
+LinkedBST::void add(int root,int data){
+	
+}
+
+LinkedBST::void preordertraversal(){
+
+}
+
+LinkedBST::bool search(int data){
+
+}
--- linkedBST.h
+++ linkedBST.h
@@ -4,14 +4,24 @@ class Node{
 	int data;
 	Node* left;
 	Node* right;
+
+	Node(){
+		data =0;
+		left=NULL;
+		right=NUll;
+	}
+
 };
+d
 class LinkedBST: public BST{
 	private:
 		Node* root;
 	public:
+		LinkedBST(){
+			root = 0;
+		}
 		void add(int data);
 		void preordertraversal();
 		bool search(int data);
-		bool search(int data);
 };
-//i am a programmer
+
