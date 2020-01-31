cfa183a9873dbfde5dfd6a5fc4372fb30a8ed1d3
Bisheshwor Neupane <eroj.nyoupane2@gmail.com>
2020-01-31 15:23:51 +0545

    after adding functions of binarysearchtree,add and preordertraversal.

--- linkedBST.cpp
+++ linkedBST.cpp
@@ -1,16 +1,83 @@
-#include<iostream>
-#include "linkedBST.cpp"
+#include "linkedBST.h"
+#include <iostream>
 
+void LinkedBST::add(int data){
+}
 
-
-LinkedBST::void add(int root,int data){
+void LinkedBST::add(Node* root,int data){
+	if(root->data == 0){
+		root->data = data;
+	}
+	else
+	{
+		if(data< root->data){
+			if(root->left == NULL){
+				Node* newNode = new Node();
+				newNode->data=data;
+				root->left=newNode;
+			}
+			else{
+				add(root,data);
+		}
+			
+		}
+		
+		else if(data> root->data){
+			if(root->right == NULL){
+				Node* newNode = new Node();
+				newNode->data=data;
+				root->right=newNode;
+			}
+			
+		}
+		else{
+			add(root,data);
+		}
+	}
 	
 }
 
-LinkedBST::void preordertraversal(){
 
+
+bool LinkedBST::search(int data){
+	return;
 }
 
-LinkedBST::bool search(int data){
+bool LinkedBST::search(Node* root,int data){
+	if(root->data == 0){
+		return False;
+	}
+	elseif{
+		if(data<root->data){
+			search(root->left,data);
+		}
+		elseif(data>root->data){
+			search(root->right,data);
+		}
+		elseif(data==root->data){
+			std::cout<<data<<"found in "<<root<<std::endl;
+			return True;
 
+		}
+		else{
+			std::cout<<data<<" Not found"<<endl;
+			return False;
+		}
+	}
 }
+
+void LinkedBST::preordertraversal(){
+	return;
+}
+void LinkedBST::preordertraversal(Node* root){
+	if(root->data==0){
+		return;
+	}
+	
+	cout<<root->data<<endl;
+	preordertraversal(root->left);
+	preordertraversal(root->right);
+	
+}
+
+
--- linkedBST.h
+++ linkedBST.h
@@ -1,18 +1,25 @@
 #include "BST.h"
+#include<iostream>
 
 class Node{
+	public:
 	int data;
 	Node* left;
 	Node* right;
-
 	Node(){
 		data =0;
-		left=NULL;
-		right=NUll;
+		left = NULL;
+		right = NULL;
+	}
+	Node(int data){
+		this->data = data;
+		left = NULL;
+		right = NULL;
 	}
 
+	~Node(){}
 };
-d
+
 class LinkedBST: public BST{
 	private:
 		Node* root;
@@ -20,8 +27,13 @@ class LinkedBST: public BST{
 		LinkedBST(){
 			root = 0;
 		}
+		~LinkedBST(){}
 		void add(int data);
+		void add(Node* root, int data);
 		void preordertraversal();
+		void preordertraversal(Node* root);		
 		bool search(int data);
+		bool search(Node* root,int data);
+
 };
 
