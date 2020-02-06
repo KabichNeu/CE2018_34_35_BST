41766a05f0a5622a01e86f616ff8ac648d91c8d0
Shreyam <sp02434318@student.ku.edu.np>
2020-02-06 09:44:46 +0545

    final project

--- BST.h
+++ BST.h
@@ -1,15 +1,13 @@
-#ifndef BST_H
-#define BST_H 
-
-
+#ifndef BST_h
+#define BST_h
 
 class BST{
-	
-public: 
+	public:
 		virtual void add(int data)=0;
-		virtual void preordertraversal()=0; 
+		virtual void preOrderTraversal(int index)=0;
 		virtual bool search(int data)=0;
 		
+		virtual int max()=0;
 };
 
 #endif
