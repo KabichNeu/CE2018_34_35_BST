ebe3afad22ad99f807faa355e562eabfc43b5171
kabichneu <kabichneu@gmail.com>
2020-02-06 09:53:27 +0545

    safdas

--- BST.h
+++ BST.h
@@ -1,14 +1,16 @@
-#ifndef BST_h
-#define BST_h
+#ifndef BST_H
+#define BST_H 
+
+
 
 class BST{
-	public:
+	
+public: 
 		virtual void add(int data)=0;
-		virtual void preOrderTraversal(int index)=0;
+		virtual void preordertraversal()=0; 
 		virtual bool search(int data)=0;
-		virtual int max()=0;
+		virtual int min()=0;
 		
-		virtual int max()=0;
 };
 
 #endif
